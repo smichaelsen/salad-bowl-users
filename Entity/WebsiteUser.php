593c4475a50f93e879a95e54756c4ea0e952f375
Sebastian Michaelsen <sebastian@michaelsen.io>
2017-09-04 23:55:52 +0200

    Work on plugin system

---
+++ Entity/WebsiteUser.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Smichaelsen\SaladBowlUsers\Entity;
+
+abstract class WebsiteUser
+{
+
+    /**
+     * @Id @Column(type="integer") @GeneratedValue
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $activationToken = '';
+
+    /**
+     * @Column(type="string", unique=true)
+     * @var string
+     */
+    protected $email;
+
+    /**
+     * @Column(type="string", unique=true)
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     *
+     */
+    public function createActivationToken()
+    {
+        $this->activationToken = bin2hex(openssl_random_pseudo_bytes(32));
+    }
+
+    /**
+     * @return string
+     */
+    public function getActivationToken()
+    {
+        return $this->activationToken;
+    }
+
+    /**
+     *
+     */
+    public function unsetActivationToken()
+    {
+        $this->activationToken = '';
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
